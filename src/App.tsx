0c4a4d180c064b55cdafae53b491a47f25d940be
Ruben Mate <79601967+rubenmate@users.noreply.github.com>
2022-07-27 22:52:29 +0200

    feat: add basic operations to todo list -Add todos -Remove todos -Filter todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 
 type Task = {
     id: number;
@@ -14,41 +14,107 @@ const TASKS: Task[] = [
     { id: 4, done: false, name: "Acabar la TodoApp" },
 ];
 
-const Task: React.FC<{ task: Task }> = ({ task }) => {
+let id: number = TASKS.length;
+
+const Task: React.FC<{
+    task: Task;
+    handleTodoDelete: Function;
+    handleCheckboxChange: Function;
+}> = ({ task, handleTodoDelete, handleCheckboxChange }) => {
     return (
         <li className="flex">
-            <input type="checkbox" checked={task.done}></input>
+            <input
+                type="checkbox"
+                checked={task.done}
+                onChange={() => handleCheckboxChange(task.id)}
+            ></input>
             <p>{task.name}</p>
-            <button className="ml-5 p-1">Delete</button>
+            <button className="ml-5 p-1" onClick={() => handleTodoDelete(task.id)}>
+                Delete
+            </button>
         </li>
     );
 };
 
-const TaskList: React.FC<{ taskList: typeof TASKS }> = ({ taskList }) => {
+const TaskList: React.FC<{
+    taskList: typeof TASKS;
+    setList: React.Dispatch<React.SetStateAction<Task[]>>;
+    filterText: string;
+}> = ({ taskList, setList, filterText }) => {
+    function handleTodoDelete(index: number) {
+        setList(taskList.filter(({ id }) => id !== index));
+    }
+
+    function handleCheckboxChange(id: number) {
+        const index = taskList.findIndex((item) => item.id === id);
+
+        const newTaskList = [...taskList];
+        newTaskList.at(index)!.done = newTaskList.at(index)?.done ? false : true;
+        setList(newTaskList);
+    }
+
     return (
         <ul>
-            {taskList.map((task: Task) => (
-                <Task key={task.id} task={task} />
-            ))}
+            {taskList
+                .filter((task) => task.name.toLowerCase().match(filterText.toLowerCase()))
+                .map((task: Task) => (
+                    <Task
+                        key={task.id}
+                        task={task}
+                        handleTodoDelete={handleTodoDelete}
+                        handleCheckboxChange={handleCheckboxChange}
+                    />
+                ))}
         </ul>
     );
 };
 
-const NewTaskForm = () => {
+const NewTaskForm: React.FC<{
+    taskList: Task[];
+    newTaskText: string;
+    onNewTaskTextChange: React.Dispatch<React.SetStateAction<string>>;
+    setList: React.Dispatch<React.SetStateAction<Task[]>>;
+}> = ({ taskList, setList, newTaskText, onNewTaskTextChange }) => {
+    function handleTodoAdd() {
+        id += 1;
+        setList([...taskList, { id: id, done: false, name: newTaskText }]);
+    }
     return (
         <form>
-            <input type="text" placeholder="New task" />
-            <button>+</button>
+            <input
+                type="text"
+                placeholder="New task"
+                value={newTaskText}
+                onChange={(e) => onNewTaskTextChange(e.target.value)}
+            />
+            <button
+                onClick={(e) => {
+                    e.preventDefault();
+                    handleTodoAdd();
+                    onNewTaskTextChange("");
+                }}
+            >
+                +
+            </button>
         </form>
     );
 };
 
-const SearchFilterTaskForm = () => {
+const SearchFilterTaskForm: React.FC<{
+    filterText: string;
+    onFilterTextChange: React.Dispatch<SetStateAction<string>>;
+}> = ({ filterText, onFilterTextChange }) => {
     return (
         <div className="flex">
-            <form>
-                <input type="text" placeholder="Search tasks..." />
-            </form>
+            <input
+                type="text"
+                placeholder="Search tasks..."
+                value={filterText}
+                onChange={(e) => {
+                    onFilterTextChange(e.target.value);
+                }}
+                onSubmit={(e) => e.preventDefault()}
+            />
             <div>
                 Filter by tags <button>Important</button> <button>Not Important</button>{" "}
                 <button>Daily</button>
@@ -59,12 +125,21 @@ const SearchFilterTaskForm = () => {
 
 function App() {
     const [list, setList] = useState(TASKS);
+    const [filterText, setFilterText] = useState("");
+    const [newTaskText, setNewTaskText] = useState("");
+    console.log(list);
+
     return (
         <>
             <h1>To-Do App</h1>
-            <SearchFilterTaskForm />
-            <TaskList taskList={list} />
-            <NewTaskForm />
+            <SearchFilterTaskForm filterText={filterText} onFilterTextChange={setFilterText} />
+            <TaskList taskList={list} setList={setList} filterText={filterText} />
+            <NewTaskForm
+                taskList={list}
+                setList={setList}
+                newTaskText={newTaskText}
+                onNewTaskTextChange={setNewTaskText}
+            />
         </>
     );
 }
